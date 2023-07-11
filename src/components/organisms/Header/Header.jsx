046822caafbcb39fe23781161659878e4aee54bb
Aldiney Moreira <aldiney.mr@gmail.com>
2023-07-11 19:27:52 -0300

    feat: hamburger menu effect

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,20 +1,22 @@
-import "./header.scss"
+import React, { useState } from 'react';
 import logo from "../../../assets/images/logo.svg";
+
+import "./header.scss";
 
 export default function Header(props) {
   const DEFAULT_MENU_ITEMS = [
     {
       text: 'About',
       link: 'https://www.youtube.com/results?search_query=readme+front+end+mentor'
-    }, 
+    },
     {
       text: 'Careers',
       link: '#'
-    }, 
+    },
     {
       text: 'Events',
       link: '#'
-    }, 
+    },
     {
       text: 'Products',
       link: '#'
@@ -24,25 +26,40 @@ export default function Header(props) {
       link: '#'
     }
   ];
+
   const menuItems = props.menuItems || DEFAULT_MENU_ITEMS;
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header className="header">
       <div className="container">
         <nav className="header__nav">
           <picture className="header__picture">
-            <img className="header__logo" src={logo} alt="Logo"></img>
+            <img className="header__logo" src={logo} alt="Logo" />
           </picture>
-          <div class="header__hamburger">
-            <span class="header__bar"></span>
-            <span class="header__bar"></span>
-            <span class="header__bar"></span>
+          <div className={`header__menu ${isOpen ? 'active' : ''}`}>
+            <ul className="header__items container">
+              {menuItems.map((item, index) => (
+                <li key={index}>
+                  <a className="header__item" href={item.link}>
+                    {item.text}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
-          <ul className="header__items">
-            {menuItems.map((item, index) => (
-              <li key={index}><a className="header__item" href={item.link}>{item.text}</a></li>
-            ))}
-          </ul>
+          <div
+            className={`header__hamburger ${isOpen ? 'active' : ''}`}
+            onClick={toggleMenu}
+          >
+            <span className="header__bar"></span>
+            <span className="header__bar"></span>
+            <span className="header__bar"></span>
+          </div>
         </nav>
         <h1 className="header__title">Immersive experiences that deliver</h1>
       </div>
