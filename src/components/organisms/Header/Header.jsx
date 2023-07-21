8811965d07b8fe97572c106dbc1dc6efe0aa9241
Aldiney Moreira <aldiney.mr@gmail.com>
2023-07-20 22:16:09 -0300

    refactor: remove redundancy in NavMenu component call

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(props) {
   return (
     <header className="header">
       <div className="container">
-        <NavMenu hamburger={true} />
+        <NavMenu hamburger />
         <h1 className="header__title">Immersive experiences that deliver</h1>
       </div>
     </header>
