2bd02224123f05c5778d5fd88be1086f86362992
Aldiney Moreira <aldiney.mr@gmail.com>
2023-07-28 21:15:06 -0300

    feat: add hover effect

--- src/components/organisms/OurCreations/OurCreations.jsx
+++ src/components/organisms/OurCreations/OurCreations.jsx
@@ -13,46 +13,46 @@ export default function OurCreations(props) {
     <section className="our-creations container">
       <h2 className="our-creations__title text--h2 text--black">Our creations</h2>
       <div className="card-list">
-        <div className="card-list__item" style={{background: `url(${image_deepearth}) center/cover`}}>
+        <a className="card-list__item" style={{background: `url(${image_deepearth}) center/cover`}}>
           <div className="card-list__background">
             <p className="card-list__title text--h3">Deep <br /> earth</p>
           </div>
-        </div>
-        <div className="card-list__item" style={{background: `url(${image_nightarcade}) center/cover`}}>
+        </a>
+        <a className="card-list__item" style={{background: `url(${image_nightarcade}) center/cover`}}>
           <div className="card-list__background">
             <p className="card-list__title text--h3">Night <br /> arcade</p>
           </div>
-        </div>
-        <div className="card-list__item" style={{background: `url(${image_soccerteam}) center/cover`}}>
+        </a>
+        <a className="card-list__item" style={{background: `url(${image_soccerteam}) center/cover`}}>
           <div className="card-list__background">
             <p className="card-list__title text--h3">Soccer <br /> team VR</p>
           </div>
-        </div>
-        <div className="card-list__item" style={{background: `url(${image_grid}) center/cover`}}>
+        </a>
+        <a className="card-list__item" style={{background: `url(${image_grid}) center/cover`}}>
           <div className="card-list__background">
             <p className="card-list__title text--h3">The <br /> grid</p>
           </div>
-        </div>
-        <div className="card-list__item" style={{background: `url(${image_fromabove}) center/cover`}}>
+        </a>
+        <a className="card-list__item" style={{background: `url(${image_fromabove}) center/cover`}}>
           <div className="card-list__background">
             <p className="card-list__title text--h3">From up <br /> above VR</p>
           </div>
-        </div>
-        <div className="card-list__item" style={{background: `url(${image__pocketborealis}) center/cover`}}>
+        </a>
+        <a className="card-list__item" style={{background: `url(${image__pocketborealis}) center/cover`}}>
           <div className="card-list__background">
             <p className="card-list__title text--h3">Pocket <br /> borealis</p>
           </div>
-        </div>
-        <div className="card-list__item" style={{background: `url(${image__curiosity}) center/cover`}}>
+        </a>
+        <a className="card-list__item" style={{background: `url(${image__curiosity}) center/cover`}}>
           <div className="card-list__background">
             <p className="card-list__title text--h3">The <br /> curiosity</p>
           </div>
-        </div>
-        <div className="card-list__item" style={{background: `url(${image__fisheye}) center/cover`}}>
+        </a>
+        <a className="card-list__item" style={{background: `url(${image__fisheye}) center/cover`}}>
           <div className="card-list__background">
             <p className="card-list__title text--h3">Make it <br /> fisheye</p>
           </div>
-        </div>
+        </a>
       </div>
       <button className="our-creations__button text--button text--black">See all</button>
     </section>
